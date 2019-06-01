63189e13042f8d9598610a8b8d245c3a655ac032
Chris Adams <chris@improbable.org>
2019-06-01 16:32:53 -0400

    Replace legacy date-parser with python-dateutil
    Since we have this as a dependency anyway there’s
    no point in reinventing this particular wheel

--- simple_cloud_site/site.py
+++ simple_cloud_site/site.py
@@ -2,10 +2,11 @@
 
 from __future__ import absolute_import, print_function, unicode_literals
 
-import datetime
 import os
 import sqlite3
 from configparser import RawConfigParser
+
+from dateutil.parser import parse as parse_date
 
 from .files import find_html_files
 from .html import Page
@@ -42,36 +43,7 @@ def load_site(base_dir=None):
 
 
 # See http://bugs.python.org/issue19065
-def tz_aware_timestamp_adapter(val):
-    datepart, timepart = val.split(b" ")
-    year, month, day = map(int, datepart.split(b"-"))
-
-    if b"+" in timepart:
-        timepart, tz_offset = timepart.rsplit(b"+", 1)
-        if tz_offset == b"00:00":
-            tzinfo = datetime.timezone.utc
-        else:
-            hours, minutes = map(int, tz_offset.split(b":", 1))
-            tzinfo = datetime.timezone(datetime.timedelta(hours=hours, minutes=minutes))
-    else:
-        tzinfo = None
-
-    timepart_full = timepart.split(b".")
-    hours, minutes, seconds = map(int, timepart_full[0].split(b":"))
-
-    if len(timepart_full) == 2:
-        microseconds = int("{:0<6.6}".format(timepart_full[1].decode()))
-    else:
-        microseconds = 0
-
-    val = datetime.datetime(
-        year, month, day, hours, minutes, seconds, microseconds, tzinfo
-    )
-
-    return val
-
-
-sqlite3.register_converter("timestamp", tz_aware_timestamp_adapter)
+sqlite3.register_converter("timestamp", parse_date)
 
 
 class PageCache(object):
